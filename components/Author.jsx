7c89559c5e6ad78317f7566899d0e2ba92a68f0a
Kygan <kygandao23@gmail.com>
2022-02-06 20:06:09 -0800

    added author information on posts

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import Image from 'next/image'
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-      <div>
-          Author
+      <div className='relative p-6 mt-20 mb-10 text-center bg-green-900 rounded-lg shadow-xl bg-opacity-10'>
+
+        <div className='absolute left-0 right-0 -top-10'>
+          <Image src={author.photo.url} alt={author.name} unoptimized height='75px' width='75px' className='align-middle rounded-full '/>
+          
+        </div>
+
+        <h3 className='my-4 text-xl text-black'>{author.name}</h3>
+        <p className='text-base text-black'>{author.bio}</p>
+        
       </div>
   );
 };
