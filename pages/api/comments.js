e642c36e2f6d17d709f808cbba0a18e30223af44
Kygan <kygandao23@gmail.com>
2022-02-08 14:53:05 -0800

    completed comments functionality, working on featured articles

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,24 +1,28 @@
 //this information will be treated as an API endpoint /api/comments
 
-import { GraphQLClient, gql } from 'graphql'
+import { GraphQLClient, gql } from 'graphql-request';
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export default async function comments(req, res) {
-    const { name, email, slug, comment } = req.body
-    const graphQLClient = new GraphQLClient(graphqlAPI, {
-        headers: {
-            authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`
-        }
-    })
+  const graphQLClient = new GraphQLClient((graphqlAPI), {
+    headers: {
+      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+    },
+  });
 
-    const query = gql`
-        mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
-            createComment(data: { name: $name, email: $email, comment: $comment, post: { connect: { slug: $slug } } }) { id }
-        }
-    `
+  const query = gql`
+    mutation CreateComment($name: String!, $email: String!, $comment: String!, $slug: String!) {
+      createComment(data: {name: $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}) { id }
+    }
+  `;
 
-    const result = await graphQLClient.request(query, req.body)
+  const result = await graphQLClient.request(query, {
+    name: req.body.name,
+    email: req.body.email,
+    comment: req.body.comment,
+    slug: req.body.slug,
+  });
 
-    return res.status(200).send(result)
+  return res.status(200).send(result);
 }
